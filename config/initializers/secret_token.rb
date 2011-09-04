a1d9ea1a2b2166a3b81d58070f0a98692b826887
Lecky Lao <leckylao@gmail.com>
2011-09-04 21:53:18 +1000

    init rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveShare::Application.config.secret_token = '59e693a5beb951ea1e828795e50e51cef7b1b2a88bc52801c00060120d76e1373c67151adc60ec9f71853fdc996cef3f911137ea6f42a023970fe52651d3016e'
